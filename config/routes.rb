bf582582a7219d58b56944c122bce1fa43697dbe
davekoder <davekoder@gmail.com>
2022-01-14 10:13:38 +0800

    updated with views, a controller, a navbar with links and a little styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'application#hello'
+  root             'static_pages#home'
+  get 'help'    => 'static_pages#help'
+  get 'about'   => 'static_pages#about'
+  get 'contact' => 'static_pages#contact'
+  get 'signup'  => 'users#new'
 end
